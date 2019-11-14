1ef5da40305f6ceeb79e81aef2a339a661518ad3
apc2 <117368291@umail.ucc.ie>
2019-11-14 15:44:52 +0000

    codeGenerator.c

--- codeGenerator/codeGenerator.c
+++ codeGenerator/codeGenerator.c
@@ -26,6 +26,7 @@ float function
 
 #include <stdio.h>
 #include <string.h>
+#include<stdlib.h>
 
 void main() {
 	FILE *fp;
@@ -35,29 +36,43 @@ void main() {
 	fscanf(fp, "%s", buffer);
 	int string_compare = strcmp(buffer, "=");
 	while (string_compare != 0) {
-		operator_check(buffer);
+		checker(buffer);
 		fscanf(fp, "%s", buffer);
 		string_compare = strcmp(buffer, "=");
 	}
 	fclose(fp);
 }
 
-void operator_check(char buffer[100]) {
+void checker(char buffer[100]) {
 	char *current_operator;
 	int add_comparison = strcmp(buffer, "+");
 	int sub_comparison = strcmp(buffer, "-");
 	int mul_comparison = strcmp(buffer, "*");
 	int div_comparison = strcmp(buffer, "/");
 	if (add_comparison == 0) {
-		current_operator = "+";
+		current_operator = "ADD";
+		printf("%s\n", current_operator);
 	} else if (sub_comparison == 0) {
-		current_operator = "-";
+		current_operator = "SUB";
+		printf("%s\n", current_operator);
 	} else if (mul_comparison == 0) {
-		current_operator = "*";
+		current_operator = "MUL";
+		printf("%s\n", current_operator);
 	} else if (div_comparison == 0) {
-		current_operator = "/";
+		current_operator = "DIV";
+		printf("%s\n", current_operator);
 	} else {
-		current_operator = "NULL";
+		int float_flag = 0;
+		char *sp;
+		for ( sp=&buffer[0]; *sp != '\0'; sp++ ) {
+			if (*sp == '.') {
+				float_flag = 1;
+			}
+		}
+		if (float_flag == 1) {
+			printf("LOADFLOAT %s\n", buffer);
+		} else {
+			printf("LOADINT %s\n", buffer);
+		}
 	}
-	printf("%s\n", current_operator);
 }
