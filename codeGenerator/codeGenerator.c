e6baf032363cfd725a78a0ad6ee3a1a83a77fe05
apc2 <117368291@umail.ucc.ie>
2019-11-19 08:49:41 +0000

    codeGenerator.c

--- codeGenerator/codeGenerator.c
+++ codeGenerator/codeGenerator.c
@@ -1,66 +1,29 @@
-/*
-for line in file
-	check if operand
-		run operand function
-	check integer
-		run integer function
-	else
-		run float function
-
-operand function
-	if +
-		append ADD
-	if -
-		append SUB
-	if *
-		append MUL
-	if /
-		append DIV
-
-integer function
-	append LOADINT + the integer
-
-float function
-	append LOADFLOAT + the float
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include<stdlib.h>
-
-void main() {
-	FILE *fp;
-	char buffer[100];
-
-	fp = fopen("sample_input", "r");
-	fscanf(fp, "%s", buffer);
-	int string_compare = strcmp(buffer, "=");
-	while (string_compare != 0) {
-		checker(buffer);
-		fscanf(fp, "%s", buffer);
-		string_compare = strcmp(buffer, "=");
-	}
-	fclose(fp);
-}
-
-void checker(char buffer[100]) {
+void checker(char buffer[100], FILE *writing_file_pointer) {
 	char *current_operator;
 	int add_comparison = strcmp(buffer, "+");
 	int sub_comparison = strcmp(buffer, "-");
 	int mul_comparison = strcmp(buffer, "*");
 	int div_comparison = strcmp(buffer, "/");
+	int eq_comparison = strcmp(buffer, "=");
 	if (add_comparison == 0) {
-		current_operator = "ADD";
-		printf("%s\n", current_operator);
+		current_operator = "ADD\n";
+		fputs(current_operator, writing_file_pointer);
+		printf("%s", current_operator);
 	} else if (sub_comparison == 0) {
-		current_operator = "SUB";
-		printf("%s\n", current_operator);
+		current_operator = "SUB\n";
+		fputs(current_operator, writing_file_pointer);
+		printf("%s", current_operator);
 	} else if (mul_comparison == 0) {
-		current_operator = "MUL";
-		printf("%s\n", current_operator);
+		current_operator = "MUL\n";
+		fputs(current_operator, writing_file_pointer);
+		printf("%s", current_operator);
 	} else if (div_comparison == 0) {
-		current_operator = "DIV";
-		printf("%s\n", current_operator);
+		current_operator = "DIV\n";
+		fputs(current_operator, writing_file_pointer);
+		printf("%s", current_operator);
 	} else {
 		int float_flag = 0;
 		char *sp;
@@ -70,9 +33,34 @@ void checker(char buffer[100]) {
 			}
 		}
 		if (float_flag == 1) {
+			fputs("LOADFLOAT ", writing_file_pointer);
+			fputs(buffer, writing_file_pointer);
+			fputs("\n", writing_file_pointer);
 			printf("LOADFLOAT %s\n", buffer);
 		} else {
+			fputs("LOADINT ", writing_file_pointer);
+			fputs(buffer, writing_file_pointer);
+			fputs("\n", writing_file_pointer);
 			printf("LOADINT %s\n", buffer);
 		}
 	}
+}
+void main() {
+	FILE *reading_file_pointer;
+	FILE *clearing_file_pointer;
+	FILE *writing_file_pointer;
+	char buffer[100];
+	reading_file_pointer = fopen("translatorOutput.txt", "r");
+	clearing_file_pointer = fopen("../virtualMachine/codeGeneratorOutput.txt", "w");
+	writing_file_pointer = fopen("../virtualMachine/codeGeneratorOutput.txt", "a");
+	fclose(clearing_file_pointer);
+	fscanf(reading_file_pointer, "%s", buffer);
+	int string_compare = strcmp(buffer, "\0");
+	while (string_compare != 0) {
+		checker(buffer, writing_file_pointer);
+		fscanf(reading_file_pointer, "%s", buffer);
+		string_compare = strcmp(buffer, "=");
+	}
+	fclose(reading_file_pointer);
+	fclose(writing_file_pointer);
 }
