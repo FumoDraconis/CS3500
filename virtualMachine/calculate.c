8372f985d0c702127797ea59c75616c858f0eed2
Tom <tom100cronin@gmail.com>
2019-11-20 10:12:51 +0000

    commented some and removed prints, fixed file so makefile reads input

--- virtualMachine/calculate.c
+++ virtualMachine/calculate.c
@@ -1,13 +1,11 @@
-//
-// Created by Tom Cronin on 18/11/2019.
-//
 
-
-
+/*
+ * doCalculation takes an operator as an int and to floats of numbers
+ * it uses a switch to determine what the operator to use
+ * it calculates and stores the result as a double
+ * it returns the double
+ * */
 double doCalculation(int operation, double number_1, double number_2) {
-        printf("\nFirst Number: %.3f ", number_1);
-        printf("Opperator (%d)",operation );
-        printf(" Second Number: %.3f", number_2);
     double result;
     switch (operation) {
         case 1: // Add
@@ -23,6 +21,5 @@ double doCalculation(int operation, double number_1, double number_2) {
             result = number_1 / number_2;
             break;
     }
-    printf(" result = %.3f\n", result);
     return result;
 }
