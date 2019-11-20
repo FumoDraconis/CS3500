cb40d0a5a62bf46798ffaf111f44c358d82e8fc4
cathal <hurryingrook21@gmail.com>
2019-11-20 15:08:45 +0000

    working tokeniser

--- tokeniser/tokeniser.c
+++ tokeniser/tokeniser.c
@@ -16,13 +16,23 @@ int checkInput(char inputEquation[100]){
 	char opt[] = "/*-+.";
 	char real_opt[] = "/*-+";
 	char fail_opt[] = "().";
-	char all_opt[] = "/*-+()";
+	char all_opt[] = "/*-+().";
+	char dot_les[] = "/*-+()";
 	int opt_check = 0;
 	int breakes_check=0;
 	int real_check = 0;
 	int lastNumber = strlen(inputEquation);
+	int ch = 0;
 	int num_brack =0;
+	int neg_check =0;  
 	for (int counter = 0; counter < strlen(inputEquation); counter += 1 ){
+		if (inputEquation[counter-1]>='0' && inputEquation[counter-1]<='9' || strchr(all_opt,inputEquation[counter-1])!= NULL ){
+			ch = 0 ;
+		}else{
+			ch =1;			
+		}
+		
+
 		if (inputEquation[counter] == '(' || inputEquation[counter] == ')'){
 			num_brack ++;
 		}
@@ -37,66 +47,82 @@ int checkInput(char inputEquation[100]){
 		}
 		if ( strlen(inputEquation) <=1) {
 			printf("invalid input:\n");
-			printf("1");			
+			printf("1");	
 			return 0;
-		}else if (isalpha(inputEquation[counter]) && inputEquation[counter] != *opt){
+		}else if (isalpha(inputEquation[counter]) && inputEquation[counter] != *opt || ch ==1  ){
 			printf("invalid input:\n");
-			printf("2");	
+			printf("2");
 			return 0;
-		}else if(strchr(opt,inputEquation[counter])!= NULL 				&&strchr(opt,inputEquation[counter +1])!= NULL ){
+		}else if(strchr(opt,inputEquation[counter])!= NULL &&strchr(opt,inputEquation[counter +1])!= NULL ){
 			printf("invalid input:\n");
-			printf("3");	
+			printf("3");
 			return 0;
-		}else if(inputEquation[counter] == '(' && isdigit(inputEquation[counter+1])==0 && inputEquation[counter+1] != '('){
+
+
+		}else if(inputEquation[counter] == '(' && isdigit(inputEquation[counter+1])==0 && inputEquation[counter+1] != '(' ){
+			if(inputEquation[counter+1]!='-'){
 			printf("invalid input:\n");
-			printf("4");	
+			printf("4");
 			return 0;
+}
+
+
 		}else if(inputEquation[0]== '.' || inputEquation[lastNumber-2 ]=='.'){
 			printf("invalid input:\n");
-			printf("5");	
+			printf("5");
+			return 0;
+
+		}else if(inputEquation[counter]== ')' && strchr(dot_les,inputEquation[counter + 1])== NULL &&inputEquation[lastNumber-2 ]!= ')' ){
+			printf("invalid input:\n");
+			printf("8");
 			return 0;
 }
 }
 	if(num_brack % 2 !=0 ){
 			printf("invalid input:\n");
-			printf("8");	
+			printf("6");
 			return 0;	
 	} 
 	return 1;	
 }
 void separator(char input[100]){
-	FILE *outputFile = fopen("translator/tokeniser_output.txt", "w");
+	FILE *outputFile = fopen("translator/tokeniserOutput.txt", "w");
 	for (int counter = 0; counter < strlen(input); counter += 1 ){
 		char *namePtr = input;
 		char data;
 		if (isdigit(input[counter])!=0 || input[counter] =='.'){
 			fputc(input[counter],outputFile);
-			printf( "%c",*(namePtr +counter));
+
+
+		
 				
                 }else if(input[counter]== ')' && input[counter+1] =='(' ){
-			printf("\n%c\n",*(namePtr +counter));
-			fprintf(outputFile,"\n%c",*(namePtr +counter));
+
+			fprintf(outputFile,"\n%c\n*\n",*(namePtr +counter));
 		
  		}else if(input[counter]== ')' ){
-			printf("\n%c",*(namePtr +counter));
 			fprintf(outputFile,"\n%c",*(namePtr +counter));		
 
 	
 		}else if (input[counter] =='(' && isdigit(input[counter-1])!=0){
-			printf("\n%c\n",*(namePtr +counter));
-			fprintf(outputFile,"\n%c\n",*(namePtr +counter));
+
+			fprintf(outputFile,"\n*\n%c\n",*(namePtr +counter));
 
                 }else if(input[counter] =='(' && isdigit(input[counter-1])==0){
-			printf("%c\n",*(namePtr +counter));
+
+
 			fprintf(outputFile,"%c\n",*(namePtr +counter));
 
-                }else { 
-			printf("\n%c\n", *(namePtr + counter));
+
+
+		}else { 
+
 			fprintf(outputFile,"\n%c\n",*(namePtr +counter));
 		}	
-	}		
+}			
 	fclose(outputFile);
 }
+
 int main( ) {
 	char inputEquation[100];
 	printf("enter EQ:");
@@ -106,6 +132,7 @@ int main( ) {
 		return 0;	
 	}
 	remove_spaces(inputEquation);
+	printf("%s",inputEquation);
 	separator(inputEquation);
 	return 0;
 }
