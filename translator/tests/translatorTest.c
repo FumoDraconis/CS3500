0e4f2a1f2a8c7d6fd81091d00e5f8cf8b2791d24
Patrick <98patrick98@gmail.com>
2019-11-26 22:19:43 +0000

    Correct outputs on tests

--- translator/tests/translatorTest.c
+++ translator/tests/translatorTest.c
@@ -19,24 +19,27 @@ TESTS {
   strncat(infixInput, "(10^2)*(5+5+5+5)/10000", 25);
   strcpy(infix1, infixInput);
 
-  ok (InfixToPostfix(infix1, postfix1) == 0, "Success");
+  ok (InfixToPostfix(infix1, postfix1) == 0, "Infix to postfix successful with correct input");
 
-  ok (push(three) == 1, "success");
+  strncat(infix1, "*", 2);
+  ok (InfixToPostfix(infix1, postfix1) == 1, "Infix to Postfix successful result with incorrect input");
+
+  ok (push(three) == 1, "Push method successful with empty stack");
   top = 1000;
-  ok (push(three) == 0, "successful");
+  ok (push(three) == 0, "Push method successful result with full stack");
   top = 0;
 
-  ok (pop() == three, "success");
-  ok (pop() == 'F', "success");
+  ok (pop() == three, "Pop successful with items in stack");
+  ok (pop() == 'F', "Pop successful result with empty stack");
 
-  ok (is_operator(mul) == 1, "Success");
-  ok (is_operator(add) == 1, "Success");
-  ok (is_operator(fail) == 0, "Success");
+  ok (is_operator(mul) == 1, "Multiply operator correct result");
+  ok (is_operator(add) == 1, "Add operator correct result");
+  ok (is_operator(fail) == 0, "Incorrect input outputing correct result");
 
-  ok (precedence(power) == 3, "Success");
-  ok (precedence(mul) == 2, "Success");
-  ok (precedence(add) == 1, "Success");
-  ok (precedence(fail) == 0, "Success");
+  ok (precedence(power) == 3, "Precedence returned correctly");
+  ok (precedence(mul) == 2, "Precedence returned correctly");
+  ok (precedence(add) == 1, "Precedence returned correctly");
+  ok (precedence(fail) == 0, "Precedence returned correctly");
 
 
 }
