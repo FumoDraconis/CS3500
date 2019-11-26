46bf533adbe1904342952f8cfa34f6ba7568d490
cathal <hurryingrook21@gmail.com>
2019-11-26 17:41:23 +0000

    real commmeting

--- tokeniser/remove_spaces.c
+++ tokeniser/remove_spaces.c
@@ -1,10 +1,16 @@
+/*
+* cathal walsh 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-//
-// removes spaces from the inputEquation enter by the user from the file
-//
+/*
+*removes spaces from the inputEquation enter by the user from the file
+*@param: input from file 
+*@returns: input without spaces
+*/
+
 char remove_spaces(char* s) {
     const char* d = s;
     do {
