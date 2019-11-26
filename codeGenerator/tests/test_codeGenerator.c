ae877cd738e352444628e796b5dcb80a3c5af52c
apc2 <117368291@umail.ucc.ie>
2019-11-26 17:41:23 +0000

    Finished proper comments

--- codeGenerator/tests/test_codeGenerator.c
+++ codeGenerator/tests/test_codeGenerator.c
@@ -1,3 +1,6 @@
+/*
+* Adam Cashman
+*/	
 #include <ctap.h>
 #include "codeGenerator.c"
 
@@ -8,5 +11,5 @@ TESTS {
 	ok(checker("\"") == 0, "this character does not work with the file");
 	ok(checker("+") == 1, "This character does work when it is supposed to");
 	ok(read_file(workingFilename) == 1, "This test succeeds as %s exists", workingFilename);
-	ok(read_file(wrongFilename) == 0, "This test fails as %s does not exist", wrongFilename);
+	ok(read_file(wrongFilename) == 0, "This test succeed as %s does not exist", wrongFilename);
 }
