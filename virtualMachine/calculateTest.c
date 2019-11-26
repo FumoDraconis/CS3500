b67fcce4dfc35317a091a295da98b9c393a8005c
Tom <tom100cronin@gmail.com>
2019-11-26 08:31:28 +0000

    added one working test

---
+++ virtualMachine/calculateTest.c
@@ -0,0 +1,21 @@
+#include <ctap.h>
+#include "calculate.h"
+
+#define add 1
+#define mul 2
+#define sub 3
+#define div 4
+
+TESTS {
+  //tests the addition case
+  ok (20 == doCalculation(add, 10.0, 10.0), "10 + 10 does equal 20");
+
+  //tests the subtraction case
+  ok (30 == doCalculation(sub, 45.0, 15.0), "45 - 15 does equal 30");
+
+  //tests the multiplication case
+  ok (7 == doCalculation(mul, 3.5, 2.0), "3.5 * 2.0 does equal 7");
+
+  //tests the division case
+  ok (32 == doCalculation(div, 128, 4), "128 / 4 does equal 32");
+}
