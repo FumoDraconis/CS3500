556a5286b17b4a6137a5db9ed5a09db8eb494e95
apc2 <117368291@umail.ucc.ie>
2019-11-26 16:24:52 +0000

    Done testing and commenting

---
+++ codeGenerator/codeGenerator.h
@@ -0,0 +1,9 @@
+#ifdef CS3500_CODEGENERATOR_H
+#define CS3500_CODEGENERATOR_H
+
+	int write_operator(char *operator);
+	int write_float(char buffer[100]);
+	int write_int( char buffer[100]);
+	int checker(char buffer[100]);
+	int read_file(char *filename);
+#endif
