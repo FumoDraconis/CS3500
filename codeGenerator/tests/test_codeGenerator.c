eeb97ea693d57a8dc31898986e3dbc06a042875e
apc2 <117368291@umail.ucc.ie>
2019-11-26 19:26:09 +0000

    Working with %

--- codeGenerator/tests/test_codeGenerator.c
+++ codeGenerator/tests/test_codeGenerator.c
@@ -2,7 +2,7 @@
 * Adam Cashman
 */	
 #include <ctap.h>
-#include "codeGenerator.c"
+#include "../codeGenerator.c"
 
 char *workingFilename = "translatorOutput.txt";
 char *wrongFilename = "Random.txt";
@@ -10,6 +10,7 @@ char *wrongFilename = "Random.txt";
 TESTS {
 	ok(checker("\"") == 0, "this character does not work with the file");
 	ok(checker("+") == 1, "This character does work when it is supposed to");
+	ok(checker("%") == 1, "% works");
 	ok(read_file(workingFilename) == 1, "This test succeeds as %s exists", workingFilename);
 	ok(read_file(wrongFilename) == 0, "This test succeed as %s does not exist", wrongFilename);
 }
